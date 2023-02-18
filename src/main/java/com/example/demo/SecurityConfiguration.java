bed527dc7e5500a788d7e86fa645b8454fe2e148
gitDeveloper2 <kamaujosephhorace@gmail.com>
2023-02-18 11:54:37 +0300

    before checkout

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,31 +1,62 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private CustomOauthService customOauthService;
+
+
+    public CustomOauthService getCustomOauthService() {
+        return customOauthService;
+    }
+
+    public void setCustomOauthService(CustomOauthService customOauthService) {
+        this.customOauthService = customOauthService;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(a->
-                        a.antMatchers("/","/error","/webjars/**").permitAll()
+                        a.antMatchers("/","/error","/login","/oauth/**","/webjars/**").permitAll()
                                 .anyRequest().authenticated()
                         )
                 .exceptionHandling(e->e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .logout(l->l.logoutSuccessUrl("/").permitAll())
                 .csrf(c->c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .oauth2Login((f)->{
-                    f.failureHandler(((request, response, exception) -> {
-                        request.getSession().setAttribute("error.message",exception.getMessage());
-                                System.out.println("failed omce");
+                    f.userInfoEndpoint().userService(customOauthService);
+//
+                    f.successHandler(new AuthenticationSuccessHandler() {
+                        @Override
+                        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                            CustomOauthUser customOauthUser= (CustomOauthUser) authentication.getPrincipal();
 
-                    })
-                    );
+                            System.out.println(customOauthUser.getAttributes());
+                            System.out.println("sun");
+                            response.sendRedirect("/");
+                        }
+                    });
                 });
     }
 }
